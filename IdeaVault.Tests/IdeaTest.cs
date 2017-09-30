4b2a825407b719a438fe234e8ace65d82ff634dc
Davis Plumlee <davis.plumlee@gmail.com>
2017-09-29 19:53:28 -0600

    Adds tests for Idea Controller

--- IdeaVault.Tests/IdeaTest.cs
+++ IdeaVault.Tests/IdeaTest.cs
@@ -11,15 +11,39 @@ namespace IdeaVault.Tests
     {
 
         private IQueryEngine _query = Substitute.For<IQueryEngine>();
+        
 
         [Fact]
-        public void Create()
+        public void CreateIdea()
         {
-            
+            _query.CreateItemAsync(SampleIdea).Returns(SampleIdea);
+            var ideaController = new IdeaController(_query);
+            var result = ideaController.AddIdea(SampleIdea);
+            Assert.Same(SampleIdea, result);
+        }
+
+        [Fact]
+        public void GetIdea()
+        {
+            _query.GetItem<Idea>(SampleIdea.Id).Returns(SampleIdea);
+            var ideaController = new IdeaController(_query);
+            var result = ideaController.GetIdea(SampleIdea.Id);
+            Assert.Same(SampleIdea, result);
+        }
+
+        [Fact]
+        public void GetIdeas()
+        {
+            var SampleIdeas = new List<Idea>{SampleIdea};
+            _query.GetItems<Idea>().Returns(SampleIdeas);
+            var ideaController = new IdeaController(_query);
+            var result = ideaController.GetIdeas();
+            Assert.Same(SampleIdeas, result);
         }
 
         private static Idea SampleIdea = new Idea
         {
+            Id = "123abc",
             Content = "Sample Content",
             Date = new DateTime(),
             Comments = new List<Comment>()
