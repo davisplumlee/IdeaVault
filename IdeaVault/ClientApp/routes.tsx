993d6f1a2ef4d96ae63668c6794e36a102282110
Davis Plumlee <davis.plumlee@gmail.com>
2017-10-17 23:02:09 -0600

    Adds routing for detail view

--- IdeaVault/ClientApp/routes.tsx
+++ IdeaVault/ClientApp/routes.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Dashboard } from './components/Dashboard';
-import { Idea } from './components/entry/Idea'
+import { Idea } from './components/entry/Idea';
+import { Entry } from './components/detail/Entry';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Dashboard } />
     <Route path='/add' component={ Idea } />
+    <Route path='/idea' component={ Entry } />
 </Layout>;
